6e9eb7b65dab5643c3c051b2481eb9d6e11decf3
Joaquin12334 <156699225+Joaquin12334@users.noreply.github.com>
2024-01-22 12:26:33 -0500

    Create gestor.py

---
+++ gestor.py
@@ -0,0 +1,12 @@
+import random 
+
+elements = "+-/*!&$#?=@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+lon = int(input("Escribe la longitud"))
+
+password = "" 
+
+for i in range(lon):
+    password += random.choice(elements)
+
+print(password)
